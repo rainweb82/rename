0fc155501061fc42319d33f9fce22c4d1d920d9a
芬达泡泡 <iamruirui@gmail.com>
2022-06-13 02:56:53 +0000

    update run.sh.

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source ./config
 rm -rf watchdog
-git clone --depth 1 $hub watchdog
+echo 正在更新脚本，请稍后...
+git clone --depth 1 $hub watchdog --quiet
 cp ./watchdog/run.sh run.sh
 bash ./watchdog/watchdog.sh
 ​
