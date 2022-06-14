212ade5e7925597599527db93647446377a9eefb
芬达泡泡 <iamruirui@gmail.com>
2022-06-14 08:09:09 +0000

    update watchdog.sh.

--- watchdog.sh
+++ watchdog.sh
@@ -69,7 +69,7 @@ result=$(echo "$strA" | egrep -o "(<font color=#FF0000>)(.*)(font>)")
 ipp="`echo ${result:20:$((${#result}-27))}`"
 uipp="`urlEncode $ipp`"
 }
-autograph="%3Cbr%3E%3Cbr%3E%E6%9C%AC%E9%80%9A%E7%9F%A5+By%EF%BC%9Ahttps%3A%2F%2Fgithub.com%2Frainweb82%2Fwatchdog"
+autograph="%3Cbr+%2F%3E%3Cbr+%2F%3E%E6%9C%AC%E9%80%9A%E7%9F%A5+By%EF%BC%9Agithub.com%2Frainweb82%2Fwatchdog"
 #检测代码开始
 zcnum=0
 cwnum=0
@@ -124,7 +124,7 @@ do
 	#判断是否发送每日推送
 	if [ $nowtime -eq $daypost ] && [ $issend -eq 1 ]
 	then
-		nowmsg=\&title=$surl%E7%9B%91%E6%8E%A7%E6%97%A5%E6%8A%A5\&content=%E7%9B%91%E6%8E%A7%E5%9F%9F%E5%90%8D%EF%BC%9A$surl%3Cbr+%2F%3E%E7%B4%AF%E8%AE%A1%E7%9B%91%E6%8E%A7%EF%BC%9A$(($zcnum+$cwnum))%E6%AC%A1+%E3%80%90%E6%AD%A3%E5%B8%B8%EF%BC%9A$zcnum%E6%AC%A1%EF%BC%8C%E9%94%99%E8%AF%AF%EF%BC%9A$cwnum%E6%AC%A1%E3%80%91%3Cbr+%2F%3E%E8%BF%90%E8%A1%8C%E6%97%B6%E9%97%B4%EF%BC%9A$day%E5%A4%A9$hour%E5%B0%8F%E6%97%B6$min%E5%88%86$sec%E7%A7%92%3Cbr+%2F%3E`date +"%m-%d_%H:%M:%S"`%3Cbr%3E%E7%BD%91%E7%BB%9C%E5%BD%92%E5%B1%9E%EF%BC%9A$uipp%3Cbr%3E$wrong$autograph\&template=html
+		nowmsg=\&title=$surl%E7%9B%91%E6%8E%A7%E6%97%A5%E6%8A%A5\&content=%E7%9B%91%E6%8E%A7%E5%9F%9F%E5%90%8D%EF%BC%9A$surl%3Cbr+%2F%3E%E7%B4%AF%E8%AE%A1%E7%9B%91%E6%8E%A7%EF%BC%9A$(($zcnum+$cwnum))%E6%AC%A1+%E3%80%90%E6%AD%A3%E5%B8%B8%EF%BC%9A$zcnum%E6%AC%A1%EF%BC%8C%E9%94%99%E8%AF%AF%EF%BC%9A$cwnum%E6%AC%A1%E3%80%91%3Cbr+%2F%3E%E8%BF%90%E8%A1%8C%E6%97%B6%E9%97%B4%EF%BC%9A$day%E5%A4%A9$hour%E5%B0%8F%E6%97%B6$min%E5%88%86$sec%E7%A7%92%3Cbr+%2F%3E%E7%BD%91%E7%BB%9C%E5%BD%92%E5%B1%9E%EF%BC%9A$uipp%3Cbr+%2F%3E`date +"%m-%d_%H:%M:%S"`%3Cbr+%2F%3E$wrong$autograph\&template=html
 		if [ ! $pushplustokena ]
 		then
 			echo -e "\033[34m"未设置PUSHPLUS-A，跳过本次每日推送任务
@@ -181,13 +181,13 @@ do
 		#更新连续错误计数，超过指定次数更新域名时使用
 		lxcwhj=$(($lxcwhj+1))
 		#记录错误日志，以备每日推送时使用
-		wrong="%3Cbr%3E%E4%BB%A3%E7%A0%81%EF%BC%9A$code+%E6%97%B6%E9%97%B4%EF%BC%9A`date +"%m-%d_%H:%M:%S"`$wrong"
+		wrong="%3Cbr+%2F%3E%E4%BB%A3%E7%A0%81%EF%BC%9A$code+%E6%97%B6%E9%97%B4%EF%BC%9A`date +"%m-%d_%H:%M:%S"`$wrong"
 		#打印错误文字
 		echo -e "\033[31m"网站异常,内容无指定文字 代码:$code $date
 		#判断是否需要推送
 		if [ $(( $times % $msgtimes )) = 0 ]  && [ $times -ne 0 ] ; then
 			#推送消息
-			nowmsg=\&title=$surl%E7%BD%91%E7%AB%99%E6%8C%82%E4%BA%86\&content=$surl+%E5%9F%9F%E5%90%8D%E6%8C%82%E4%BA%86%EF%BC%8C%E5%BF%AB%E5%8E%BB%E7%9C%8B%E7%9C%8B+`date +"%m-%d_%H:%M:%S"`%3Cbr%3E%E7%BD%91%E7%BB%9C%E5%BD%92%E5%B1%9E%EF%BC%9A$uipp$autograph\&template=html
+			nowmsg=\&title=$surl%E7%BD%91%E7%AB%99%E6%8C%82%E4%BA%86\&content=$surl+%E5%9F%9F%E5%90%8D%E6%8C%82%E4%BA%86%EF%BC%8C%E5%BF%AB%E5%8E%BB%E7%9C%8B%E7%9C%8B%3Cbr+%2F%3E%E7%BD%91%E7%BB%9C%E5%BD%92%E5%B1%9E%EF%BC%9A$uipp%3Cbr+%2F%3E`date +"%m-%d_%H:%M:%S"`$autograph\&template=html
 			if [ ! $pushplustokena ]
 			then
 				echo -e "\033[34m"未设置PUSHPLUS-A，跳过本次错误推送任务
